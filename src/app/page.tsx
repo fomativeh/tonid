ec569de7275337e1e594c768441c16da5486b38b
fomativeh <ufomaitive@gmail.com>
2024-04-03 16:53:45 +0100

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div></div>
+    <main className='w-full min-h-[100vh] flex justify-center items-center bg-theme-light-blue'>
+      <span className='bg-black p-[30px] rounded-[20px] text-white'>Welcome to TON Identity.</span>
+    </main>
   )
 }
 
